82d4ac5b51d47b94de5e01828650119d2804c8d6
wioenena <wioenena@gmail.com>
2026-07-14 07:14:04 +0300

    build(warnings): simplify MSVC warning flags to /W4 and /WX
    Remove the granular /w14xxx flags in favor of relying on /W4 and /WX
    alone for MSVC warning enforcement.

--- CompilerWarnings.cmake
+++ CompilerWarnings.cmake
@@ -29,24 +29,6 @@ set(GCC_WARNINGS
 set(MSVC_WARNINGS
     /W4
     /WX
-    /w14242
-    /w14254
-    /w14263
-    /w14265
-    /w14287
-    /w14296
-    /w14311
-    /w14545
-    /w14546
-    /w14547
-    /w14549
-    /w14555
-    /w14619
-    /w14640
-    /w14826
-    /w14905
-    /w14906
-    /w14928
 )
 
 if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
